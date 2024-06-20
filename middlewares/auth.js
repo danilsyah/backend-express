d947309eb0def9339d8e40cafcc98c0d108c7fe6
danilsyah <danilsyaharihardjo@gmail.com>
2024-06-20 07:49:15 +0700

    005 membuat Restful API get all data users

--- middlewares/auth.js
+++ middlewares/auth.js
@@ -6,14 +6,14 @@ const jwt = require('jsonwebtoken');
 
 const verifyToken = (req, res, next) => {
     // Get token
-    const token = req.header['authorization'];
+    const token = req.headers['authorization'];
 
     if (!token) return res.status(401).json({message: 'Unauthenticated'});
 
     // verify token
     jwt.verify(token, process.env.JWT_SECRET, (err, decoded) => {
         if (err) return res.status(401).json({message: 'Invalid token'});
-        req.userId = devoded.id;
+        req.userId = decoded.id;
         next();
     });
 };
